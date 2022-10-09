3a696e162603ee7e9689d85c85c523f75fd214f7
jacobef <jacobef2@gmail.com>
2022-10-09 13:11:57 -0600

    clarify some stuff

--- lec_frontend_drafting/urls.py
+++ lec_frontend_drafting/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path
-
-from lec_frontend_drafting import settings
-
 
 def example_view(request):
     return render(request, "example_page.html")
